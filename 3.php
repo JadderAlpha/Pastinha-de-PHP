fbbb0dcf5a0a66c0e4ce0965b78c37f52eb99625
Acnologla <35548489+Acnologla@users.noreply.github.com>
2018-10-30 14:33:27 -0300

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,10 @@
+<?php
+$anacleto = 150;
+$felisberto =110;
+while (true){
+    $anacleto+=2;
+    $felisberto+=3;
+if ($felisberto > $anacleto){
+    echo "O felisberto tem maior idade, felisberto: ".$felisberto." Anacleto: ".$anacleto;
+    return;}
+}
